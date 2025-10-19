ba4e763f9dca2adea1b6a460c0af566edfcd0dd7
nuelo <akalo.emmanuel18@gmail.com>
2025-10-19 09:37:32 +0100

    refact: improve time display ui

--- profile-card-t0/script.js
+++ profile-card-t0/script.js
@@ -4,4 +4,4 @@ const time = document.querySelector("[data-testid='test-user-time']")
 
 ;
 
-setInterval(() => (time.textContent = `${Date.now()}ms`), 1000);
+setInterval(() => (time.textContent = ` ${Date.now()}ms`), 1000);
